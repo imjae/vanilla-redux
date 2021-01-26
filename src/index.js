07a4a0c2b02764222fd3e3f3f0abd2d0fe9e0938
imjae <code@kakao.com>
2021-01-26 21:54:42 +0900

    리덕스 사용하는 count 예제로 변경

--- src/index.js
+++ src/index.js
@@ -1,27 +1,32 @@
-const add = document.getElementById("add");
+import { createStore } from "redux";
+// createStore는 데이터를 넣는 곳 생성 
+// 데이터 관리
+const MINUS = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
 
-number.innerText = count;
+
+const conutModifier = (count  = 0, action) => {
+  if(action.type === "ADD") {
+    return count + 1;
+  } else if(action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count ;
+  }
+};
+
+const countStore = createStore(conutModifier);
+
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "MINUS"});
+console.log(countStore.getState());
 
 // 데이터가 변경되었을 때 바닐라는 updateText함수와같이
-// 데이터가 ㅂ녀경되었다고 알려주기 위한 함수를 사용해야한다.
+// 데이터가 변경되었다고 알려주기 위한 함수를 사용해야한다.
 // 이것이 리덕스가 멋진 이유중 하나다.
-const updateText = () => {
-  number.innerText = count;
-}
-
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-};
-
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
